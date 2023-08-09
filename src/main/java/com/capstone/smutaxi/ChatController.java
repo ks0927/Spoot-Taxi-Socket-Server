5906ee753ae9c6fce85b4b4d8b2ba940867485ff
ks0927 <brian_als@naver.com>
2023-08-09 18:13:06 +0900

    Feat: Stomp 채팅 컨트롤러추가

---
+++ src/main/java/com/capstone/smutaxi/ChatController.java
@@ -0,0 +1,34 @@
+package com.capstone.smutaxi;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ApiClient apiClient;
+    private final IDGenerator idGenerator;
+
+    @MessageMapping("/send")
+    public void userChat(UserMessage message) {
+        message.setId(idGenerator.getNextId());
+        Message sendUserMessage = apiClient.saveUserMessage(message);
+        messagingTemplate.convertAndSend("/sub/channel/" + message.getChatRoom().getId(), sendUserMessage);
+    }
+
+    @MessageMapping("/exit")
+    public void exitChat(SystemMessage systemMessage) {
+        systemMessage.setId(idGenerator.getNextId());
+        systemMessage.setMessage(systemMessage.getSenderName() + "님이 나갔습니다.");
+        systemMessage.setIsSystem(true);
+
+        Message sendExitMessage = apiClient.saveSystemMessage(systemMessage);
+        messagingTemplate.convertAndSend("/sub/channel/" + systemMessage.getChatRoom().getId(), sendExitMessage);
+    }
+}

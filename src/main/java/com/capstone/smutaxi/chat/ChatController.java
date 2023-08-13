2f473618365afcf17029fe20278d2ed1f1e31002
ks0927 <brian_als@naver.com>
2023-08-14 01:30:50 +0900

    Feat: Rest API -> 카프카로 변경

--- src/main/java/com/capstone/smutaxi/chat/ChatController.java
+++ src/main/java/com/capstone/smutaxi/chat/ChatController.java
@@ -1,10 +1,10 @@
 package com.capstone.smutaxi.chat;
 
 import com.capstone.smutaxi.ApiClient;
+import com.capstone.smutaxi.KafkaProducerService;
 import com.capstone.smutaxi.utils.IDGenerator;
-import com.capstone.smutaxi.domain.Message;
-import com.capstone.smutaxi.domain.SystemMessage;
-import com.capstone.smutaxi.domain.UserMessage;
+import com.capstone.smutaxi.entity.SystemMessage;
+import com.capstone.smutaxi.entity.UserMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -19,21 +19,24 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ApiClient apiClient;
     private final IDGenerator idGenerator;
+    private final KafkaProducerService kafkaProducerService;
 
     @MessageMapping("/send")
     public void userChat(UserMessage message) {
         message.setId(idGenerator.getNextId());
-        Message sendUserMessage = apiClient.saveUserMessage(message);
-        messagingTemplate.convertAndSend("/sub/channel/" + message.getChatRoom().getId(), sendUserMessage);
+        //Message sendUserMessage = apiClient.saveUserMessage(message);
+        kafkaProducerService.sendUserMessage(message);
+        messagingTemplate.convertAndSend("/sub/channel/" + message.getChatRoom().getId(), message);
     }
 
     @MessageMapping("/exit")
-    public void exitChat(SystemMessage systemMessage) {
-        systemMessage.setId(idGenerator.getNextId());
-        systemMessage.setMessage(systemMessage.getSenderName() + "님이 나갔습니다.");
-        systemMessage.setIsSystem(true);
+    public void exitChat(SystemMessage message) {
+        message.setId(idGenerator.getNextId());
+        message.setMessage(message.getSenderName() + "님이 나갔습니다.");
+        message.setIsSystem(true);
 
-        Message sendExitMessage = apiClient.saveSystemMessage(systemMessage);
-        messagingTemplate.convertAndSend("/sub/channel/" + systemMessage.getChatRoom().getId(), sendExitMessage);
+        //Message sendExitMessage = apiClient.saveSystemMessage(systemMessage);
+        kafkaProducerService.sendSystemMessage(message);
+        messagingTemplate.convertAndSend("/sub/channel/" + message.getChatRoom().getId(), message);
     }
 }

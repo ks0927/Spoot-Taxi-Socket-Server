21876dc71af69f994015f75a6f327a2902817eb0
ks0927 <brian_als@naver.com>
2023-08-14 01:28:48 +0900

    Feat: 카프카 config 추가

---
+++ src/main/java/com/capstone/smutaxi/KafkaProducerConfig.java
@@ -0,0 +1,50 @@
+package com.capstone.smutaxi;
+
+import com.capstone.smutaxi.entity.SystemMessage;
+import com.capstone.smutaxi.entity.UserMessage;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String servers;
+
+    @Bean
+    public ProducerFactory<String, UserMessage> userMessageProducerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+    @Bean
+    public ProducerFactory<String, SystemMessage> systemMessageProducerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, UserMessage> kafkaTemplateUserMessage() {
+        return new KafkaTemplate<>(userMessageProducerFactory());
+    }
+    @Bean
+    public KafkaTemplate<String, SystemMessage> kafkaTemplateSystemMessage() {
+        return new KafkaTemplate<>(systemMessageProducerFactory());
+    }
+}
